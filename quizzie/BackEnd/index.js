a2768087e6c293a8c20b2b42131499d506842684
hsingh936 <113837323+hsingh936@users.noreply.github.com>
2024-02-09 15:36:15 +0530

    Update index.js

--- quizzie/BackEnd/index.js
+++ quizzie/BackEnd/index.js
@@ -3,47 +3,36 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-
 require('dotenv').config();
-
 
 const app = express();
 
-
-app.use(cors({
-  origin: '*',
-  methods: ["POST", "GET","DELETE","PUT"],
-  credentials: true
-}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect(process.env.MONGODB_URL)
-
-  .then(() => console.log('MongoDB connected successfully'))
-  .catch((error) => console.log(error));
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB connection established successfully');
-});
-
-
+// Define your routes before the CORS middleware
 const authRoutes = require('../BackEnd/routes/auth');
 const quizRoutes = require('../BackEnd/routes/quiz');
 app.use('/auth', authRoutes);
 app.use('/quiz', quizRoutes);
 
+// Apply the CORS middleware
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Welcome');
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch((error) => console.log(error));
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB connection established successfully');
 });
 
+app.get('/', (req, res) => {
+    res.send('Welcome');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
-
-
