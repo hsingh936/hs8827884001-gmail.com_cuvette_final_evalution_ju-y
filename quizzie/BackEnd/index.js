3dfd8964f375a96d42dfbf8d2301d53ab677201c
hsingh936 <113837323+hsingh936@users.noreply.github.com>
2024-02-09 12:59:42 +0530

    Update index.js

--- quizzie/BackEnd/index.js
+++ quizzie/BackEnd/index.js
@@ -3,20 +3,19 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-
 require('dotenv').config();
-
 
 const app = express();
 
-
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect(process.env.MONGODB_URL)
-
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log('MongoDB connected successfully'))
   .catch((error) => console.log(error));
 
@@ -25,21 +24,26 @@ connection.once('open', () => {
   console.log('MongoDB connection established successfully');
 });
 
-
 const authRoutes = require('../BackEnd/routes/auth');
 const quizRoutes = require('../BackEnd/routes/quiz');
 app.use('/auth', authRoutes);
 app.use('/quiz', quizRoutes);
-
 
 app.get('/', (req, res) => {
   res.send('Welcome');
 });
 
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
