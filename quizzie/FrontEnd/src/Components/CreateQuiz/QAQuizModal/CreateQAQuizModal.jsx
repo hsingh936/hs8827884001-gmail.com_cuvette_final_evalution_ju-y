36e986ca1ccebe3564a7a088541f363c94e00e5d
hsingh936 <113837323+hsingh936@users.noreply.github.com>
2024-02-09 14:09:30 +0530

    Update CreateQAQuizModal.jsx

--- quizzie/FrontEnd/src/Components/CreateQuiz/QAQuizModal/CreateQAQuizModal.jsx
+++ quizzie/FrontEnd/src/Components/CreateQuiz/QAQuizModal/CreateQAQuizModal.jsx
@@ -54,7 +54,7 @@ const CreateQAQuizModal = ({ quizData, userId }) => {
       const authToken = localStorage.getItem('authToken');
 
       const response = await axios.post(
-        'https://quizapi-nine.vercel.app/quiz/create',
+        'https://apiquiz.vercel.app/quiz/create',
         {
           userId: userId,
           quizName: quizData.quizName,
