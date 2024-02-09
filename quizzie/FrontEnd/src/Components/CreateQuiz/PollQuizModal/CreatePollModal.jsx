da276e36c4328cb8fc457feefa0d72f6a16b4e39
hsingh936 <113837323+hsingh936@users.noreply.github.com>
2024-02-09 12:42:28 +0530

    Update CreatePollModal.jsx

--- quizzie/FrontEnd/src/Components/CreateQuiz/PollQuizModal/CreatePollModal.jsx
+++ quizzie/FrontEnd/src/Components/CreateQuiz/PollQuizModal/CreatePollModal.jsx
@@ -45,7 +45,7 @@ const CreatePollModal = ({ quizData, userId }) => {
       const authToken = localStorage.getItem('authToken');
 
       const response = await axios.post(
-        'https://quizapi-five.vercel.app/quiz/create',
+        'https://quizzieapi.vercel.app/quiz/create',
         {
           userId: userId,
           quizName: quizData.quizName,
