cf59cf356e1cdeab6ee7938324385d56e0699dd1
hsingh936 <113837323+hsingh936@users.noreply.github.com>
2024-02-09 13:04:37 +0530

    Update Analytics.jsx

--- quizzie/FrontEnd/src/Components/Analytics/Analytics.jsx
+++ quizzie/FrontEnd/src/Components/Analytics/Analytics.jsx
@@ -25,7 +25,7 @@ const AnalyticsPage = ({ userId }) => {
   const fetchQuizzes = useCallback(async () => {
     try {
       const authToken = localStorage.getItem('authToken');
-      const response = await axios.get(`https://quizzieapi.vercel.app/quiz/user/${userId}`, {
+      const response = await axios.get(`https://quizapi-nine.vercel.app/quiz/user/${userId}`, {
         headers: {
           Authorization: `Bearer ${authToken}`,
         },
@@ -54,7 +54,7 @@ const AnalyticsPage = ({ userId }) => {
   const handleConfirmDelete = async () => {
     try {
       const authToken = localStorage.getItem('authToken');
-      await axios.delete(`https://quizzieapi.vercel.app/quiz/${quizToDelete}`, {
+      await axios.delete(`https://quizapi-nine.vercel.app/quiz/${quizToDelete}`, {
         headers: {
           Authorization: `Bearer ${authToken}`,
         },
