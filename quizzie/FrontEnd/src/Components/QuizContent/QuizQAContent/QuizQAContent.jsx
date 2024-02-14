ea71b69ab9d49a5ed7e53f8823fd26cbf544b9eb
hsingh936 <113837323+hsingh936@users.noreply.github.com>
2024-02-14 14:55:22 +0530

    Update QuizQAContent.jsx

--- quizzie/FrontEnd/src/Components/QuizContent/QuizQAContent/QuizQAContent.jsx
+++ quizzie/FrontEnd/src/Components/QuizContent/QuizQAContent/QuizQAContent.jsx
@@ -70,23 +70,24 @@ const QAQuizContentPage = ({ quiz }) => {
     }
   };
 
-  const handleOptionSelect = (optionIndex) => {
-    setSelectedOption(optionIndex);
+const handleOptionSelect = (optionIndex) => {
+  setSelectedOption(optionIndex);
 
-    if (timer === 0) {
-      const currentQuestion = quiz.questions[currentQuestionIndex];
+  if (timer === 0) {
+    const currentQuestion = quiz.questions[currentQuestionIndex];
+    const selectedOptionData = currentQuestion.options[optionIndex];
 
-      if (currentQuestion?.options[optionIndex].isCorrect) {
-        setCorrectAnswers((prevCorrectAnswers) => prevCorrectAnswers + 1);
-      }
-
-      if (!isLastQuestion) {
-        handleNextQuestion();
-      } else {
-        handleSubmitQuiz();
-      }
+    if (selectedOptionData.isCorrect) {
+      setCorrectAnswers((prevCorrectAnswers) => prevCorrectAnswers + 1);
     }
-  };
+
+    if (!isLastQuestion) {
+      handleNextQuestion();
+    } else {
+      handleSubmitQuiz();
+    }
+  }
+};
 
   if (quiz.questions.length === 0) {
     return <div className={styles.noQuestions}>No questions available</div>;
